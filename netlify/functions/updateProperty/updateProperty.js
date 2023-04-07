479b5990fbe589cf6684947d76a0dfd3eff8ba75
Ruben Hernandez <rubenhnt@gmail.com>
2023-04-06 19:08:44 -0500

    add update property function

---
+++ netlify/functions/updateProperty/updateProperty.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+const mongoClient = new MongoClient(process.env.REACT_APP_DB_CONNECTION);
+
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+  try {
+    const data = JSON.parse(event.body);
+    const database = (await clientPromise).db(process.env.REACT_APP_DB_NAME);
+    const collection = database.collection("vacantproperties");
+    const result = await collection.updateOne(
+      {
+        _id: event.queryStringParameters("_id"),
+      },
+      data
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+module.exports = { handler };
